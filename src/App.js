eaaa4208984aa1c1e00189cbc646d2bfa08b5b53
Busra Yuksel <busraayukselll@gmail.com>
2021-12-13 22:02:46 +0300

    Creating files and project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import AboutUs from '../main/pages/aboutUs';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <AboutUs />
     </div>
   );
 }
